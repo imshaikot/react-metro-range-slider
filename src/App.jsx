09ddd0c95e6d39a4284e54db47ce57ebc807d0b8
Shariar Shaikot <mail2shariar@gmail.com>
2017-12-24 14:52:03 +0600

    added: basic api for external usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import Range from './components/Range';
+import ReactRangeSlider from './components/ReactRangeSlider';
 
-const App = () => (
-  <Range className="slider" />
-);
+class App extends React.Component {
+
+  state = {value: 0};
+
+  componentWillMount() {
+    // setInterval(() => {
+    //   this.setState({
+    //     value: this.state.value + 1
+    //   });
+    // }, 500)
+  }
+  render() {
+    return (
+      <div>
+        <ReactRangeSlider onChange={(e, v) => console.log(v)} value={this.state.value} max={500} preValue={10} />
+        <ReactRangeSlider onChange={(e, v) => console.log(v)} value={this.state.value} max={500} preValue={100} />
+      </div>
+    );
+  }
+}
 
 export default App;
